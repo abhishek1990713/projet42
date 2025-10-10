67e8c06e33beb5cd6c352e9eee0b56875318bab7
ABHISHEK SHARMA <80061192+abhishek1990713@users.noreply.github.com>
2025-10-10 13:45:31 +0530

    Update ass.py

--- ass.py
+++ ass.py
@@ -20,3 +20,21 @@ class FeedbackResponse(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+feedback_data = schemas.FeedbackResponse(
+    id=0,
+    correlation_id=x_correlation_id,
+    application_id=x_application_id,
+    document_id=x_document_id,
+    file_id=x_file_id,
+    authorization_coin_id=x_authorization_coin,
+    feedback_response=payload,
+    feedback_source=x_feedback_source,
+    created_by=x_created_by,
+    created_on=datetime.now(),
+    field_count=stats.get("field_count", 0),
+    positive_count=stats.get("positive_count", 0),
+    negative_count=stats.get("negative_count", 0),
+    percentage=stats.get("percentage", 0.0)
+)
